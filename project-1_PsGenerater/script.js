2bb47eaf4181ee480491a7c133c78fb699d36063
Ashwini-Khunte <160710313+Ashwini-Khunte@users.noreply.github.com>
2024-02-26 11:23:46 +0000

    Added Node.js

--- project-1_PsGenerater/script.js
+++ project-1_PsGenerater/script.js
@@ -1,48 +1,37 @@
 const input = document.querySelector(".random-ps");
-const generateBtn = document.querySelector(".gnerate-btn");
+const generateBtn = document.querySelector(".gnerate-ps");
 const copyBtn = document.querySelector(".copy-ps");
 
-
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefgfijklmnopqrstuvwxyz";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
-const symbols = "!@#$%^&*()_=-'.;/><,.?{}[]|";
+const symbols = "!@#$%^&*()_-=|}{[]:;'><?/.,"
 
-const allChars = upperCase + lowerCase + symbols + numbers;
+const allChars = upperCase + lowerCase + numbers + symbols;
 
-let length = 12;
+let psLength = 8;
 
-function generatePs() {
+let generatePs = () => {
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += numbers[Math.floor(Math.random() * numbers.length)];
-    password += symbols[Math.floor(Math.random() * symbols.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];                                                
 
-    for(let i = 0; i < length; i++) {
-        password += allChars[Math.floor(Math.random() * allChars.length)];
-    }
-
+    for(let i = 0; i < psLength; i++) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+    } 
     input.value = password;
 }
 
-generateBtn.addEventListener("click",generatePs);
+generateBtn.addEventListener("click", generatePs);
 
-// copyBtn.addEventListener("click", () => writeClipboardText(input.value));
-// async function writeClipboardText(text) {
-//   try {
-//     await navigator.clipboard.writeText(text);
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
-
-copyBtn.addEventListener("click", () => copiedPs(input.value));
-
-async function copiedPs(text) {
+async function copyText(text) {
     try {
-        await navigator.clipboard.writeText(input.value);
+        await navigator.clipboard.writeText(text);
     } catch (error) {
         console.error(error.message);
     }
 }
+
+copyBtn.addEventListener("click", () => copyText(input.value));
